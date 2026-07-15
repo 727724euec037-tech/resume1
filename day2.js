3735234ee24ae41dda315e660a73e0469a6181a0
727724euec037 <727724euec037@skcet.ac.in>
2026-07-15 09:58:06 +0530

    resume task1

---
+++ day2.js
@@ -0,0 +1,47 @@
+// function balcheck(value)
+// {
+//     let bal=value;
+// if(bal>=1000)
+// {
+//     return "payment successfull"
+// }
+// else if(bal>1)
+// {
+//     return "payment failed"
+// }
+// return "need some amount"
+// }
+// // balcheck(500)
+// // balcheck(1500)
+// let returngift=balcheck(500)
+// console.log(returngift);
+// let bal2=function()
+// {
+//     return "this is variable function"
+// }
+// bal2()
+// // ----------------------------------
+// let bal3=()=>{
+//     return "arrow function"
+// }
+// bal3()
+//looping
+function fruits()
+{
+    let apple=10
+for(let i=0;i<5;i++)
+{
+    apple-=2;
+    console.log("Apple left:"+apple);
+}
+}
+fruits()
+  let bal=value;
+if(bal>=1000)
+ {
+     return "payment successfull"
+ }
+while(apple>10)
+{
+
+}
---
+++ script.js
@@ -0,0 +1,73 @@
+// let a=20
+// let b=30
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// let age=20
+// console.log(age>10);
+// console.log(age<10);
+// console.log(age==10);
+// console.log(age!=10);
+// let ag=19
+// let voterid=false
+// console.log(ag >=18 && voterid==true);
+// console.log(ag >=18 || voterid==true);
+// let islan=true
+// console.log(!islan);
+// let ticket = true
+// if(ticket == true)
+// {
+//     console.log("you can watch the movie");
+// }
+// else{
+//     console.log("you cannot watch the movie");
+// }
+// let signal="green"
+// if(signal=="red")
+// {
+//     console.log("stop");
+// }
+//  else if(signal=="green")
+// {
+//     console.log("go");
+// }
+//  else if(signal=="yellow")
+// {
+//     console.log("wait");
+// }
+// else
+// {
+//     console.log("signal is not working");
+// }
+// let department="ECE"
+// switch(department)
+// {
+//     case "EEE":
+//         console.log("EEE BLOCK")
+//         break;
+//     case "ECE":
+//         console.log("ECE BLOCK")
+//         break;
+//     default:
+//         console.log("Invalid department")
+// }
+console.log("-------------------------------")
+let mark=100
+if(mark>=95)
+{
+    console.log("A grade");
+}
+else if(mark>=75)
+{
+    console.log("B grade");
+}
+else if(mark>=55)
+{
+    console.log("C grade");
+}
+else
+{
+    console.log("Fail");
+}
